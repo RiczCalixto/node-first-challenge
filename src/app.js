ff235f4b816eb9582abf1ba256b0fcf81b79c5fc
Ricardo Calixto <rcalixtos@gmail.com>
2020-04-10 01:47:04 -0300

    Finish first test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,76 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, techs, url } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    techs,
+    url,
+    likes: 0,
+  };
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, techs, url } = request.body;
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({ error: "erro!" });
+  }
+
+  const newRepo = {
+    id,
+    title,
+    techs,
+    url,
+    likes: repositories[repositoryIndex].likes,
+  };
+
+  repositories[repositoryIndex] = newRepo;
+
+  return response.json(newRepo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({ error: "erro!" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).json({});
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({ error: "erro!" });
+  }
+
+  repositories[repositoryIndex].likes += 1;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
